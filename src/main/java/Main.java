9eb1a161a8033e85d838ca88195feb31fb587fc7
BuildTools <unconfigured@null.spigotmc.org>
2023-03-10 01:57:34 +0300

    First commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,29 @@
 // dev branch for Y.Practicum
+import java.util.Scanner;
 public class Main {
 
+
     public static void main(String[] args) {
-        // ваш код начнется здесь
-        // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-        System.out.println("Привет Мир");
+        Scanner scanner = new Scanner(System.in);
+        int guestInput;
+
+        while(true) {
+
+            System.out.println("На скольких человек необходимо разделить счёт?");
+            guestInput = scanner.nextInt();
+            if(guestInput > 1) {
+                System.out.println("all ok");
+                break;
+            }
+            if (guestInput == 1 ) {
+                System.out.println("Не на кого делить, вы кушали один, к сожалению придется оплатить счет именно вам :)");
+            }
+            if (guestInput < 1) {
+                System.out.println("Некорректное значение для подсчёта");
+            }
+
+        }
+
+        scanner.close();
     }
 }
