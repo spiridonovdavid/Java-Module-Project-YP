88667c5a43b4cf9f6532bc408b8f0f80338eed01
BuildTools <unconfigured@null.spigotmc.org>
2023-03-12 21:51:36 +0300

    Реализация классов - ProductList - Calculator - Formatter

--- src/main/java/Formatter.java
+++ src/main/java/Formatter.java
@@ -1,2 +1,20 @@
 public class Formatter {
+    double totalPrice;
+
+    public String ruble(double totalPrice) {
+        this.totalPrice = totalPrice;
+        if ((int)this.totalPrice == 1) {
+            return " рубль";
+        }
+        if ((int)this.totalPrice >= 2 && (int)this.totalPrice <= 4) {
+            return " рубля";
+        }
+        return " рублей";
+    }
+
+    String roundPrice(double productPrice) {
+        return String.format("%.2f", productPrice);
+    }
+
+
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,26 +4,71 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+
         int guestInput;
+        ProductList ProductList = new ProductList();
+        Calculator Calculator = new Calculator();
+        Formatter Formatter = new Formatter();
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("На скольких человек необходимо разделить счёт?");
 
         while(true) {
-
-            System.out.println("На скольких человек необходимо разделить счёт?");
-            guestInput = scanner.nextInt();
-            if(guestInput > 1) {
-                System.out.println("all ok");
-                break;
-            }
-            if (guestInput == 1 ) {
-                System.out.println("Не на кого делить, вы кушали один, к сожалению придется оплатить счет именно вам :)");
-            }
-            if (guestInput < 1) {
+            if(!scanner.hasNextInt()) {
+                System.out.println("Некорректный ввод количества человек, повторите попытку");
+                scanner.next();
+            } else {
+                guestInput = scanner.nextInt();
+                if(guestInput > 1) {
+                    break;
+                }
                 System.out.println("Некорректное значение для подсчёта");
             }
+        }
+        System.out.println("Счет необходимо разделить на " + guestInput + " человек.");
 
+        Calculator.countPerson = guestInput;
+        while(true) {
+            System.out.println("Введите название товара или команду Завершить");
+            ProductList.productName = scanner.next();
+
+            if (ProductList.productName.equalsIgnoreCase("Завершить")) {
+                break;
+            }
+            System.out.println("Введите стоимость блюда");
+            while(!scanner.hasNextDouble()) {
+                System.out.println("Некорректный ввод цены, повторите попытку");
+                scanner.next();
+            }
+
+            ProductList.productPrice = scanner.nextDouble();
+
+            while(ProductList.productPrice <= 0) {
+                System.out.println("Некорректная цена блюда, введите корректную цену");
+                while(!scanner.hasNextDouble()) {
+                    System.out.println("Некорректный ввод цены, повторите попытку");
+                    scanner.next();
+                }
+                ProductList.productPrice = scanner.nextDouble();
+            }
+
+            ProductList.totalProductList = ProductList.addToProductList(ProductList.productName, ProductList.productPrice);
+            Calculator.totalPrice = Calculator.totalPrice + ProductList.productPrice;
+            System.out.println("Товар успешно добавлен в счет");
         }
 
+        if (Calculator.totalPrice <= 0) {
+            System.out.println("Вы ничего не заказывали, платить не за что");
+        } else {
+
+            System.out.println("Добавленные товары: \n" + ProductList.totalProductList);
+            System.out.println(
+                    "Каждый должен заплатить по "
+                    + Formatter.roundPrice(Calculator.calcPricePerPerson(Calculator.countPerson, Calculator.totalPrice))
+                    + Formatter.ruble(Calculator.calcPricePerPerson(Calculator.countPerson, Calculator.totalPrice))
+            );
+        }
         scanner.close();
     }
 }
